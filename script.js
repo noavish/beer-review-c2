fe93feb71843dd0e94f38d01562f7e9ef3e30307
noavish <noavish1@gmail.com>
2017-12-29 11:09:11 +0200

    final version

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 $(document).ready(function () {
     var beers = [];
-
-    function addBeer (name, category, rating) {
-        beers.push({'name': name, 'category': category, 'rating': rating});
+    var isNotSortedFromBest = true;
+    
+    function addBeer(name, category, rating) {
+        beers.push({ name: name, category: category, rating: rating });
     }
 
     $('.post-beer').click(function () {
@@ -10,29 +11,24 @@ $(document).ready(function () {
         renderBeers();
     });
 
-    function renderBeers () {
+    function renderBeers() {
         $('.beers-list').find('li').remove();
-        for (var i=0; i<beers.length; i++) {
+        for (var i = 0; i < beers.length; i++) {
             $('.beers-list').append('<li>Beer Name: ' + beers[i].name + ' Beer Category: ' + beers[i].category + ' Beer Rating: ' + beers[i].rating + '</li>');
         }
     }
 
-    function apppendListItems(beerArray ,text) {
-        for (var i=0; i<beers.length; i++) {
-            $('.beers-list').append('<li>Beer Name: ' + beers[i].name + ' Beer Category: ' + beers[i].category + ' Beer Rating: ' + beers[i].rating + '</li>');
-        }  
-        $('.sort-beers').html(text);
-    }
-
-    var isNotSortedFromBest = true;
     $('.sort-beers').click(function () {
-        $('.beers-list').find('li').remove();
         if (isNotSortedFromBest) {
-            apppendListItems(beers.sort(function (a, b) {return b.rating - a.rating}), 'Sort beers from worst to best'); 
+            $('.sort-beers').html('Sort beers from worst to best');
+            beers.sort(function (a, b) { return b.rating - a.rating })
+            renderBeers();
             isNotSortedFromBest = false;
         } else {
-            apppendListItems(beers.sort(function (a, b) {return a.rating - b.rating}), 'Sort beers from Best to worst'); 
+            $('.sort-beers').html('Sort beers from Best to worst');
+            beers.sort(function (a, b) { return a.rating - b.rating })
+            renderBeers();
             isNotSortedFromBest = true;
-        } 
+        }
     });
 });
